3e18363ae58c176e7b5d4ca479afd1a7c42e1606
digitbase <digitbase@gmail.com>
2020-11-27 20:41:16 +0800

    init project

--- demo01/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-//import App from "./App";
-import Xiaojiejie from "./Xiaojiejie";
-
-ReactDOM.render(<Xiaojiejie />, document.getElementById("root"));
